0d23ba956c560951dcca4c157ce5985b18acd60e
ADANEXE <adanexe111@gmail.com>
2026-02-03 14:54:11 +0500

    Delete models directory

--- models/premium.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const premiumSchema = new mongoose.Schema({
-  guildId: String,
-  expires: Date,
-});
-
-module.exports = mongoose.model('Premium', premiumSchema);
